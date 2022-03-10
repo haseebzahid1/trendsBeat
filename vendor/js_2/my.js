45cdf36529203093f52c06f0d2150d5b0245201a
haseebzahid1 <mmianhaseeb444@gmail.com>
2022-03-10 20:37:24 +0500

    trendBeat added

--- vendor/js_2/my.js
+++ vendor/js_2/my.js
@@ -32,3 +32,52 @@
         }
     }
 });
+
+
+
+//////////////  FEATURE BLOGS OWL CAROUSEL
+$('.blog-owl-caorousel').owlCarousel({
+  items:4,
+  loop:true,
+  margin:21,
+  merge:false,
+  dots:false,
+  mergeFit:true,
+  // mergeFit:false,
+  responsive:{
+      567:{
+          mergeFit:true,
+          // mergeFit:false,
+          items:3,
+          margin:20,
+      },
+      767:{
+          mergeFit:true,
+          items:3,
+          margin:30,
+      },
+      1000:{
+        mergeFit:true,
+          // mergeFit:false,
+          items:3,
+          margin:50,
+      },
+      1400:{
+        margin:50,
+        mergeFit:true,
+        // mergeFit:false,
+          items:4,
+      }
+  }
+});
+
+
+
+
+var swiper = new Swiper(".mySwiper2", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
